c56de208af8ae7f5e40a40434c892d205a50472d
PinkJacketKid <sts122000@gmail.com>
2022-07-02 19:58:49 -0400

    Timer in progress

---
+++ BallinSeagulls/Assets/Scripts/Timer.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    public TextMeshProUGUI timeText;
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+    }
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+    void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        float milliSeconds = (timeToDisplay % 1) * 1000;
+        timeText.text = string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliSeconds);
+    }
+}
