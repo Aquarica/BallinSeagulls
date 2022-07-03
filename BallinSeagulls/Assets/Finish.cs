9711c167da1e675c0bd92ffc0fb3e13da7118e0a
aprader1 <austin@thepraders.com>
2022-07-03 11:00:30 -0400

    Basic Script and respawn WIP

---
+++ BallinSeagulls/Assets/Finish.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+
+    //Written in psuedo before fixing
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If (player is touching finish plane ie this object){
+        //    move to next track
+        //    run "Level Start" script
+        //}
+    }
+}
---
+++ BallinSeagulls/Assets/Level Start.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+//Psuedo code for now
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Teleport player to "Spawn" object
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (player is touching kill plane){
+        //      teleport back to spawn
+        //}
+    }
+}
